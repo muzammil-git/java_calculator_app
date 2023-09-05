984baf0ef56350ddd264128f55daff7fd0458257
muzammil-git <syedmuzammil67@gmail.com>
2023-09-05 22:47:10 +0500

    initial

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import com.twilio.Twilio;
 
 public class MainActivity extends AppCompatActivity {
 
